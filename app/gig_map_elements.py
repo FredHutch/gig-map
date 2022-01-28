ffa143c886206d8e037f46ffb01d425eb74b83ff
Sam Minot <sminot@fredhutch.org>
2022-01-28 08:25:30 -0800

    Add genome label

--- app/gig_map_elements.py
+++ app/gig_map_elements.py
@@ -53,16 +53,13 @@ def plot_genome_tree(fig):
         assert label_col in genome_annotations.columns.values, msg
 
         # Replace the values in the 'name' column of the DataFrame used for plotting
-        label_genomes_dict = [
-            fig.params['global']['label_genomes_by']
-        ].to_dict()
+        label_genomes_dict = genome_annotations[label_col].to_dict()
 
         # If there is no annotation, keep the original label
-        node_positions.df = node_positions.df.replace(
-            to_replace=dict(
-                name=lambda l: label_genomes_dict.get(l, l)
-            )
-        )
+        node_positions.genome_order = list(map(
+            lambda l: label_genomes_dict.get(l, l),
+            node_positions.genome_order
+        ))
 
     # Add the traces to the plot
 
@@ -99,6 +96,36 @@ def plot_genome_tree(fig):
         )
     )
 
+    # Format the axes for this plot
+    fig.log("Formatting axes for genome tree")
+    fig.subplots.format_axis(
+        id="genome_tree",
+        ax="x",
+        params=dict(
+            range=[
+                node_positions.df['x'].max() * -0.01,
+                node_positions.df['x'].max() * 1.01,
+            ]
+        )
+    )
+
+    fig.subplots.format_axis(
+        id="genome_tree",
+        ax="y",
+        params=dict(
+            tickmode="array",
+            tickvals=list(range(len(node_positions.genome_order))),
+            ticktext=[
+                l[:fig.params["global"]["max_genome_label_len"]]
+                for l in node_positions.genome_order
+            ],
+            side="right",
+            showticklabels=True,
+            automargin=True,
+        ),
+        anchor="genome_tree"
+    )
+
     fig.log("Done plotting genome tree")
 
 
@@ -119,11 +146,21 @@ def read_global(
     else:
         data['genome_annotations'] = pd.read_csv(genome_annotations)
 
+        # The table must have a column named `genome_id`
+        msg = "--genome-annotations must contain column 'genome_id'"
+        assert 'genome_id' in data['genome_annotations'].columns.values, msg
+        data['genome_annotations'].set_index("genome_id", inplace=True)
+
     # Read in the gene annotations
     if gene_annotations is None:
         data['gene_annotations'] = None
     else:
         data['gene_annotations'] = pd.read_csv(gene_annotations)
+
+        # The table must have a column named `genome_id`
+        msg = "--gene-annotations must contain column 'gene_id'"
+        assert 'gene_id' in data['gene_annotations'].columns.values, msg
+        data['gene_annotations'].set_index("gene_id", inplace=True)
 
     # Read in the gene order
     with gzip.open(gene_order, 'rt') as handle:
@@ -135,7 +172,17 @@ def read_global(
     return data
 
 
-def read_genome_tree(distmat=None):
+def plot_global(fig):
+    """Apply global plot configuration after all of the other params are set."""
+
+    # Set the global layout elements
+    fig.subplots.fig.update_layout(
+        paper_bgcolor='white',
+        plot_bgcolor='white'
+    )
+
+
+def read_genome_tree(distmat=None, **kwargs):
     """Read in data needed for the genome tree."""
 
     # Read in the distance matrix
