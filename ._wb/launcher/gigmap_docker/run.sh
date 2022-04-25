0584a69ca8b321c4680f737295b09b18d3819608
Sam Minot <sminot@fredhutch.org>
2022-04-25 11:53:11 -0700

    Add the option to stop the running task

--- ._wb/launcher/gigmap_docker/run.sh
+++ ._wb/launcher/gigmap_docker/run.sh
@@ -23,4 +23,20 @@ echo
 # Execute the tool in the local environment
 echo "Starting tool"
 echo
-/bin/bash ._wb/helpers/run_tool
+
+# Start the tool in the background
+/bin/bash ._wb/helpers/run_tool &
+
+# Get the process ID
+PID="$!"
+
+# Make a task which can kill this process
+if [ ! -d ._wb/bin ]; then mkdir ._wb/bin; fi
+echo """
+#!/bin/bash
+
+echo \"\$(date) Sending a kill signal to the process\"
+
+kill ${PID}
+""" > ._wb/bin/stop
+chmod +x ._wb/bin/stop
