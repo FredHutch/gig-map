367e67933051b1a0ec1312622216f87f3637f683
Sam Minot <sminot@fredhutch.org>
2022-06-22 16:39:43 -0700

    Try to download the GenBank record if the RefSeq doesn't exist

--- templates/fetchFTP.py
+++ templates/fetchFTP.py
@@ -6,34 +6,51 @@ import urllib.request as request
 from urllib.error import URLError
 from contextlib import closing
 
-
+# The path to download will be filled in by Nextflow
 remote_path = "${ftp_url}"
-local_path = remote_path.rsplit("/", 1)[-1]
-print(f"Downloading from {remote_path}")
-print(f"Local path is {local_path}")
 
-try:
-    with closing(
-        request.urlopen(
-            remote_path,
-            timeout=60
-        )
-    ) as r:
-        with open(local_path, 'wb') as f:
-            shutil.copyfileobj(r, f)
-except URLError as e:
-    print("The URL appears to not be valid")
-    if "${params.skip_missing_ftp}" == "true":
-        print("Missing FTP paths will be ignored")
-    else:
-        print("Raising error")
-        raise e
+# We will make a backup path which references the RefSeq record instead of GenBank
+backup_path = remote_path.replace("GCA", "GCF")
 
-# If the file is expected to be gzip compressed
-if local_path.endswith('.gz'):
 
-    # Try to open the file
-    with gzip.GzipFile(local_path, mode="r") as handle:
+def download_path(remote_path:str, skip_missing_ftp=True):
+    local_path = remote_path.rsplit("/", 1)[-1]
+    print(f"Downloading from {remote_path}")
+    print(f"Local path is {local_path}")
 
-        # Try to read it
-        handle.read()
+    try:
+        with closing(
+            request.urlopen(
+                remote_path,
+                timeout=60
+            )
+        ) as r:
+            with open(local_path, 'wb') as f:
+                shutil.copyfileobj(r, f)
+    except URLError as e:
+        print("The URL appears to not be valid")
+        if skip_missing_ftp:
+            print("Missing FTP paths will be ignored")
+            return False
+        else:
+            print("Raising error")
+            raise e
+
+    # If the file is expected to be gzip compressed
+    if local_path.endswith('.gz'):
+
+        # Try to open the file
+        with gzip.GzipFile(local_path, mode="r") as handle:
+
+            # Try to read it
+            handle.read()
+
+    return True
+
+
+# If we can't download the first option
+if not download_path(remote_path, skip_missing_ftp=False):
+
+    # Try the second option
+    print(f"Trying backup path: {backup_path}")
+    download_path(backup_path, skip_missing_ftp="${params.skip_missing_ftp}" == "true")
