dd18f9b567b44e6277fb3ce509305fc882ac11ca
Sam Minot <sminot@gmail.com>
2025-02-25 10:22:36 -0800

    Add build_pangenome.nf to BASH workbench

---
+++ ._wb/tool/build_pangenome/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+date
+echo
+echo "Running workflow from ${PWD}"
+echo
+
+# Log the parameters being used
+echo PARAMETERS
+echo
+cat ._wb/tool/params.json
+echo
+
+# Run the workflow
+echo Starting workflow
+nextflow \
+    run \
+    "${TOOL_REPO}/build_pangenome.nf" \
+    --output "${PWD}" \
+    -params-file ._wb/tool/params.json \
+    -resume \
+    -profile "${PROFILE}"
+
+# If temporary files were not placed in a separate location
+if [ -d work ]; then
+    # Delete the temporary files created during execution
+    echo Removing temporary files
+    rm -r work
+fi
+
+
+echo
+date
+echo Done
